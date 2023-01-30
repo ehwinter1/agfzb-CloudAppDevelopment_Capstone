3c905d328cca623340ac9d15d2607f1c09eb050f
Evan <ehwinter@hotmail.com>
2023-01-30 18:09:38 -0500

    Added Django Models

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,22 @@
 from django.contrib import admin
 # from .models import related models
-
+from .models import CarMake, CarModel
 
 # Register your models here.
 
 # CarModelInline class
+class CarModelInline(admin.StackedInline):
+    model = CarModel
+    extra = 0
 
 # CarModelAdmin class
+class CarModelAdmin(admin.ModelAdmin):
+    list_display = ('make', 'name', 'dealer_id', 'type', 'year')
 
 # CarMakeAdmin class with CarModelInline
+class CarMakeAdmin(admin.ModelAdmin):
+    inlines = [CarModelInline]
 
 # Register models here
+admin.site.register(CarModel)
+admin.site.register(CarMake)
--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -25,3 +25,36 @@ from django.utils.timezone import now
 
 
 # <HINT> Create a plain Python class `DealerReview` to hold review data
+
+class CarMake(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+class CarModel(models.Model):
+    make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    dealer_id = models.CharField(max_length=100)
+    type = models.CharField(
+        max_length=10,
+        choices=[("SEDAN", "Sedan"), ("SUV", "SUV"), ("WAGON", "Wagon"), ("TRUCK", "Truck")]
+    )
+    year = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.make} - {self.name}"
+
+class CarDealer:
+    def __init__(self, dealer_id, name, address):
+        self.dealer_id = dealer_id
+        self.name = name
+        self.address = address
+
+class DealerReview:
+    def __init__(self, dealer_id, rating, review, reviewer_name):
+        self.dealer_id = dealer_id
+        self.rating = rating
+        self.review = review
+        self.reviewer_name = reviewer_name
